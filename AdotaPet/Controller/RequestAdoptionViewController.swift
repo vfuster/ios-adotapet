7256a2b3e21d555100b621182dea40464e0c6739
Valeria Fuster <vfustermartins@gmail.com>
2021-04-27 17:11:53 -0300

    configured button layout request adoption

--- AdotaPet/Controller/RequestAdoptionViewController.swift
+++ AdotaPet/Controller/RequestAdoptionViewController.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class RequestAdoptionViewController: UIViewController {
     
+    @IBOutlet weak var petOwnerName: UITextField!
+    @IBOutlet weak var petOwnerPhone: UITextField!
+    @IBOutlet weak var requestAdoption: UIButton!
+    
+    @IBOutlet weak var residenceTypeTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupButtonRequestAdoption()
+    }
+    
+    func setupButtonRequestAdoption() {
+        requestAdoption.layer.cornerRadius = 20
     }
     
     
